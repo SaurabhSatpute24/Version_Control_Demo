04d56f21c32b7ab436319f839f2b455c0fc219f7
Saurabh Satpute <saurabh@email.com>
2023-05-23 13:20:11 +0530

    Snowflake configuration commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,10 @@
-package org.example;public class Main {
+package org.example;
+
+import static org.example.ConfigurationSnowflake.*;
+
+public class Main {
     public static void main(String[] args) {
+        System.out.println(session.toString());
         System.out.println("Hello world!");
     }
 }
