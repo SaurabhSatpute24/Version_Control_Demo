e6ae8790e7f93ce30ae980b17872bcf253bfac36
Saurabh Satpute <saurabh@email.com>
2023-05-23 16:06:13 +0530

    show tables camand add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,16 @@
 package org.example;
 
+import com.snowflake.snowpark_java.DataFrame;
+
 import static org.example.ConfigurationSnowflake.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(session.toString());
-        System.out.println("Hello world!");
+//        System.out.println(session.toString());
+//        System.out.println("Hello world!");
+//        System.out.println(session.getSessionStage());
+        DataFrame df =session.sql("show tables");
+        df.show();
+        session.close();
     }
 }
